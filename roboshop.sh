a16dbf155a349ff4f149cf2026a060eb9e2cf140
priyanka <info@joindevops.com>
2025-06-08 19:31:53 +0530

    colours

--- roboshop.sh
+++ roboshop.sh
@@ -6,21 +6,16 @@ ZONE_ID="Z043242732NAEK996K9L7"
 DOMAIN_NAME="priya22n.site"
 
 #for instance in ${INSTANCES[@]}
-for instance in $@
+for instance in ${INSTANCES[@]}
 do 
    
-INSTANCE_ID=$(aws ec2 run-instances \
---image-id ami-09c813fb71547fc4f \
---instance-type t3.micro \
---security-group-ids sg-01d79933c960fd126 \
---tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]"
---query "Reservations[0].Instances[0],INSTANCEId"
---output text)
+  INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01d79933c960fd126 --tag-specification "ResourceType=instance, Tags=[{Key=Name, Value=test}]" --query "Instance[0].privateIpAddress" --Output text)
 
-if [ $instance != "frontend" ]
-then
-IP=$(aws ec2 describe-instances --instance-ids INSTANCE_ID --query "Resrevations [0].Instances[0].privateIpAddress"
---output text)
+
+  if [ $instance != "frontend" ]
+  then
+ IP=$(aws ec2 describe-instances --instance-ids INSTANCE_ID --query "Resrevations [0].Instances[0].privateIpAddress"
+ --output text)
 RECORD_NAME=""$instance.$DOMAIN_NAME"
 else
 IP=$(aws ec2 describe-instances --instance-ids INSTANCE_ID --query "Resrevations [0].Instances[0].publicIpAddress"
@@ -29,10 +24,12 @@ RECORD_NAME= "$DOMAIN_NAME"
 fi
 echo "$instance IP address: $IP"
  
-aws route53 change-resource-record-sets \
+ aws route53 change-resource-record-sets \
   --hosted-zone-id $ZONE_ID\
-  --change-batch '
-  {
+--change-batch '
+
+
+{
     "Comment": "Creating OR Updating a record set for cognito endpoint"
     ,"Changes": [{
       "Action"              : "UPSERT"
