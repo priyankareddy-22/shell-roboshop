04c328827c8cfa4c3ddf874b0e0d34d17bc51db2
priyanka <info@joindevops.com>
2025-06-08 19:33:59 +0530

    colours

--- roboshop.sh
+++ roboshop.sh
@@ -9,38 +9,17 @@ DOMAIN_NAME="priya22n.site"
 for instance in ${INSTANCES[@]}
 do 
    
-  INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01d79933c960fd126 --tag-specification "ResourceType=instance, Tags=[{Key=Name, Value=test}]" --query "Instance[0].privateIpAddress" --Output text)
-
-
-  if [ $instance != "frontend" ]
+  INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.
+  micro --security-group-ids sg-01d79933c960fd126 --tag-specification "ResourceType=instance,
+  Tags=[{Key=Name, Value=test}]" --query "Instance[0].privateIpAddress" --Output text)
+   
+  if [ $instance != "frontend"]
   then
- IP=$(aws ec2 describe-instances --instance-ids INSTANCE_ID --query "Resrevations [0].Instances[0].privateIpAddress"
- --output text)
-RECORD_NAME=""$instance.$DOMAIN_NAME"
-else
-IP=$(aws ec2 describe-instances --instance-ids INSTANCE_ID --query "Resrevations [0].Instances[0].publicIpAddress"
---output text)
-RECORD_NAME= "$DOMAIN_NAME"
-fi
-echo "$instance IP address: $IP"
- 
- aws route53 change-resource-record-sets \
-  --hosted-zone-id $ZONE_ID\
---change-batch '
-
-
-{
-    "Comment": "Creating OR Updating a record set for cognito endpoint"
-    ,"Changes": [{
-      "Action"              : "UPSERT"
-      ,"ResourceRecordSet"  : {
-        "Name"              : "'$RECORD_NAME'"
-        ,"Type"             : "A"
-        ,"TTL"              : 1
-        ,"ResourceRecords"  : [{
-            "Value"         : "'$IP'"
-        }]
-      }
-    }]
-  }'
+    Ip=aws ec2 describe-instance --instance-ids $INSTANCE_ID --query "Reservations[0].
+    Instance[0].PrivateIpAddress" --output text
+  else 
+    Ip=aws ec2 describe-instance --instance-ids $INSTANCE_ID --query "Reservations[0].
+    Instance[0].PublicIpAddress" --output text
+  fi
+  echo "$instance Ip address: $IP"
 done
