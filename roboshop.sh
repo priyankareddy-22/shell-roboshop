935fa5fefce6f553d1fc3fb93d5bbabda5271b7e
priyanka <info@joindevops.com>
2025-06-08 19:37:21 +0530

    colours

--- roboshop.sh
+++ roboshop.sh
@@ -11,12 +11,12 @@ do
    
   INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.
   micro --security-group-ids sg-01d79933c960fd126 --tag-specification "ResourceType=instance,
-  Tags=[{Key=Name, Value=test}]" --query "Instance[0].privateIpAddress" --Output text)
+  Tags=[{Key=Name, Value=$instance}]" --query "Instance[0].privateIpAddress" --Output text)
    
   if [ $instance != "frontend"]
   then
-    Ip=aws ec2 describe-instance --instance-ids $INSTANCE_ID --query "Reservations[0].
-    Instance[0].PrivateIpAddress" --output text
+    Ip=$(aws ec2 describe-instance --instance-ids $INSTANCE_ID --query "Reservations[0].
+    Instance[0].PrivateIpAddress" --output text)
   else 
     Ip=aws ec2 describe-instance --instance-ids $INSTANCE_ID --query "Reservations[0].
     Instance[0].PublicIpAddress" --output text
