49916611810b861e5f7a21542065162787597551
priyanka <info@joindevops.com>
2025-06-09 21:52:36 +0530

    colours

--- roboshop.sh
+++ roboshop.sh
@@ -9,17 +9,15 @@ DOMAIN_NAME="priya22n.site"
 for instance in ${INSTANCES[@]}
 do 
    
-  INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type 
-  
-  t2.micro --security-group-ids sg-0c704820e242a6aaf --tag-specification "ResourceType=instance,
+  INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0c704820e242a6aaf --tag-specification "ResourceType=instance,
   Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --Output text)
    
   if [ $instance != "frontend" ]
   then
-    Ip=$(aws ec2 describe-instance --instance-ids $INSTANCE_ID --query "Reservations[0].
+    Ip=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].
     Instance[0].PrivateIpAddress" --output text)
   else 
-    Ip=$(aws ec2 describe-instance --instance-ids $INSTANCE_ID --query "Reservations[0].
+    Ip=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].
     Instance[0].PublicIpAddress" --output text)
   fi
   echo "$instance Ip address: $IP"
