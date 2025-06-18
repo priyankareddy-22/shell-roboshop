baa9a55618f8cc6a589404cf1fced53fd0a533a4
priyanka <info@joindevops.com>
2025-06-18 16:27:10 +0530

    Ansible Roles

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -41,16 +41,16 @@ read -s RABBITMQ_PASSWD
 cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
 VALIDATE $? "Adding rabbitmq repo"
 
-dnf install rabbitmq-server -y
+dnf install rabbitmq-server -y &>>$LOG_FILE
 VALIDATE $? "Installing rabbitmq server"
 
-systemctl enable rabbitmq-server
+systemctl enable rabbitmq-server &>>$LOG_FILE
 VALIDATE $? "Enabling rabbitmq server"
 
-systemctl start rabbitmq-server
+systemctl start rabbitmq-server &>>$LOG_FILE
 VALIDATE $? "Starting rabbitmq server"
 
-rabbitmqctl add_user roboshop RABBITMQ_PASSWD
+rabbitmqctl add_user roboshop $RABBITMQ_PASSWD
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
 
 END_TIME=$(date +%s)
